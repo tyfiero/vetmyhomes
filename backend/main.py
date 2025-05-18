151808db01c98610757d07e6f39f06fb1cfb20ce
deepanshgandhi <deepanshgandhi1404@gmail.com>
2025-05-18 08:30:53 -0700

    Merged conflicts

--- backend/main.py
+++ backend/main.py
@@ -84,7 +84,7 @@ async def test_dependencies():
 async def crew():
     return kickoff_crew(
         {
-            "query": "I want a house in Capitol hill, Seattle with 3 bedrooms and 2 bathrooms with a high walkscore"
+            "query": "Recommend houses in Capitol hill"
         }
     )
     # return kickoff_crew(
